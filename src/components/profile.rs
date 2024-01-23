bdf7038da8f6e0c9515d4b800823b1d00a3143e1
YuChiang-Chang <yuchiang.chang.taiwan@gmail.com>
2024-01-24 01:16:35 +0800

    修改css

--- src/components/profile.rs
+++ src/components/profile.rs
@@ -5,9 +5,9 @@ pub fn profile() -> Html {
     html! {
         <div id="about" class="profile">
             // <a href="javascript:void(0)" id="close-about-btn">{Html::from_html_unchecked(AttrValue::from("&Cross;"))}</a>
-            <a href="javascript:void(0)" class="profile-close-button">
-                <span class="material-symbols-rounded">{"close"}</span>
-            </a>
+            // <a href="javascript:void(0)" class="profile-close-button">
+            //     <span class="material-symbols-rounded">{"close"}</span>
+            // </a>
             // <span class="material-icons md-dark">{"face"}</span>
             // <div id="about-content">
             <div>
@@ -27,3 +27,84 @@ pub fn profile() -> Html {
         </div>
     }
 }
+
+// #[function_component(About)]
+// pub fn about() -> Html {
+    // let about_width = "20%";
+    // let adaptive_width = 1024;
+    // let focus_color = "#222222";
+
+    // let about = yew::use_node_ref(|| {
+    //     html! {
+    //         <div id="about" class="about-container">
+    //             <div class="about-content">
+    //                 </div>
+    //         </div>
+    //     }
+    // });
+
+    // let is_open = yew::use_state(|| false);
+
+    // 滑鼠懸停效果
+    // yew::use_effect_with(|| {
+    //     let about_me_btn = yew::use_ref(|| document.getElementById("aboutme-btn"));
+
+    //     fn hover_handler(ref mut about_me_btn: &mut HtmlElement) {
+    //         about_me_btn.style.backgroundColor = focus_color;
+    //     }
+
+    //     fn leave_handler(ref mut about_me_btn: &mut HtmlElement) {
+    //         about_me_btn.style.backgroundColor = about.get().style.backgroundColor.unwrap();
+    //     }
+
+    //     about_me_btn.add_event_listener("mouseover", hover_handler);
+    //     about_me_btn.add_event_listener("mouseout", leave_handler);
+
+    //     move || ()
+    // });
+
+    // 開啟側邊欄
+    // fn open_about() {
+    //     about.get().style.left = "calc(50% + 30%)";
+    //     about.get().style.width = about_width;
+    //     about.get().style.display = "block";
+    //     about.get().parent().unwrap().style.marginRight = about_width;
+    //     is_open.set(true);
+    // }
+
+    // 關閉側邊欄
+    // fn close_about() {
+    //     about.get().style.left = "100%";
+    //     about.get().style.width = "0";
+    //     about.get().style.display = "none";
+    //     about.get().parent().unwrap().style.marginRight = "0";
+    //     is_open.set(false);
+    // }
+
+    // 點擊事件
+    // fn handle_click(event: yew::MouseEvent) {
+    //     if window.innerWidth > adaptive_width {
+    //         if is_open.get() {
+    //             close_about();
+    //         } else {
+    //             open_about();
+    //         }
+    //     } else {
+    //         close_about();
+    //     }
+    // }
+
+    // html! {
+    //     <div class="about-container">
+    //         <button id="aboutme-btn" class="about-btn" onclick=handle_click>關於我</button>
+    //         {
+    //             if is_open.get() {
+    //                 about.get()
+    //             } else {
+    //                 html!{}
+    //             }
+    //         }
+    //     </div>
+    // }
+// }
+
